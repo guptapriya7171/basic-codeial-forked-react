74a586353ad66e6362bbebff5db80a27ff10b6c9
Priyanka <96413187+guptapriya7171@users.noreply.github.com>
2022-09-08 15:19:07 +0530

    Update utils.js

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,12 +1,13 @@
 export function getFormBody(params) {
   let formBody = [];
 
-  for (let property of params) {
+  for (let property in params) {
     let encodedKey = encodeURIComponent(property); //'user name' => 'user%20name'
     let encodedValue = encodeURIComponent(params[property]);
 
-    formBody(encodedKey + "=" + encodedValue);
+    formBody.push(encodedKey + "=" + encodedValue);
   }
 
   return formBody.join("&");
 }
+
