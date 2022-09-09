5f03502a63fbe3ad01ec14c778eaafc4e63c118c
Priyanka <96413187+guptapriya7171@users.noreply.github.com>
2022-09-09 16:30:10 +0530

    Delete helpers directory

--- helpers/urls.js
+++
@@ -1,8 +0,0 @@
-const API_ROOT = "https://codeial.codingninjas.com:8000/api/v2";
-
-export const APIUrl = {
-  login: () => `${API_ROOT}/users/login`,
-  signup: () => `${API_ROOT}/users/signup`,
-  fetchPosts: (page = 1, limit = 5) =>
-    `${API_ROOT}/posts?page=${page}&limit=${limit}`
-};
