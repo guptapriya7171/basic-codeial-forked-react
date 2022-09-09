9310cbe21febfcb86635123f54cb335c4b4b5aa6
Priyanka <96413187+guptapriya7171@users.noreply.github.com>
2022-09-09 16:41:08 +0530

    Update auth.js

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,25 +1,31 @@
 import {
-  LOGIN_FAILED,
   LOGIN_START,
-  LOGIN_SUCCESS
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  AUTHENTICATE_USER,
+  LOG_OUT,
+  SIGNUP_START,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILED
 } from "../actions/actionTypes";
 
-const intialAuthState = {
+const initialAuthState = {
   user: {},
   error: null,
   isLoggedin: false,
   inProgress: false
 };
 
-export default function auth(state = intialAuthState, action) {
+export default function auth(state = initialAuthState, action) {
   switch (action.type) {
     case LOGIN_START:
+    case SIGNUP_START:
       return {
         ...state,
         inProgress: true
       };
-
     case LOGIN_SUCCESS:
+    case SIGNUP_SUCCESS:
       return {
         ...state,
         user: action.user,
@@ -27,14 +33,25 @@ export default function auth(state = intialAuthState, action) {
         inProgress: false,
         error: null
       };
-
     case LOGIN_FAILED:
+    case SIGNUP_FAILED:
       return {
         ...state,
         inProgress: false,
         error: action.error
       };
-
+    case AUTHENTICATE_USER:
+      return {
+        ...state,
+        user: action.user,
+        isLoggedin: true
+      };
+    case LOG_OUT:
+      return {
+        ...state,
+        user: {},
+        isLoggedin: false
+      };
     default:
       return state;
   }
